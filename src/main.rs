743cb9a48746aa12b38b1628d7bcd954e3d32111
Konrad <szymoniak.konrad@gmail.com>
2021-03-03 22:15:21 +0100

    correct Subdocument usage in main

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use anyhow::Result;
 use std::sync::Arc;
 
 mod document;
+mod matcher;
 
 use document::{Content, Subdocument};
 
 fn main() -> Result<()> {
     let content = Arc::new(Content::new("example".into())?);
-    let subdocument_1 = Subdocument::full_document(content.clone());
-    let subdocument_2 = Subdocument::new(content.clone());
+
+    let _subdocument_1 = Subdocument::full_document(content.clone());
+    let _subdocument_2 = Subdocument::new(content.clone(), "word");
 
     Ok(())
 }
