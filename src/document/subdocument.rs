be233f0da1069d5f22b9643ce6cf2a7297ad17ce
Konrad <szymoniak.konrad@gmail.com>
2021-03-03 22:13:06 +0100

    run grep while creating subdocument

--- src/document/subdocument.rs
+++ src/document/subdocument.rs
@@ -1,4 +1,10 @@
 use crate::document::Content;
+use anyhow::Result;
+use grep::{
+    matcher::Matcher,
+    regex::RegexMatcher,
+    searcher::{sinks::UTF8, Searcher},
+};
 use std::sync::Arc;
 
 pub struct Subdocument {
@@ -14,10 +20,23 @@ impl Subdocument {
         }
     }
 
-    pub fn new(content: Arc<Content>) -> Self {
-        Self {
-            content,
-            indices: Vec::new(),
+    pub fn new(content: Arc<Content>, pattern: &str) -> Result<Self> {
+        let regex_matcher = RegexMatcher::new(pattern)?;
+        let mut searcher = Searcher::new();
+
+        let mut indices = Vec::new();
+        for index in 0..content.len() as u32 {
+            searcher.search_slice(
+                &regex_matcher,
+                content.get_line(index).as_ref(),
+                UTF8(|_, line| {
+                    regex_matcher.find(line.as_bytes())?.unwrap();
+                    indices.push(index);
+                    Ok(true)
+                }),
+            )?;
         }
+
+        Ok(Self { content, indices })
     }
 }
